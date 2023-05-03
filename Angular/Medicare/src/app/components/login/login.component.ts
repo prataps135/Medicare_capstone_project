475d44c4511992e2ddc5ec93b9ce54fd60c2c1d1
Pratap_Singh_Sisodia <prataps135@gmail.com>
2023-05-03 21:22:18 +0530

    angular components added

--- Angular/Medicare/src/app/components/login/login.component.ts
+++ Angular/Medicare/src/app/components/login/login.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  // userData;
+  // constructor(private user: UserService) {}
 
+  // ngOnInit() {
+  //   this.user.currentUserData.subscribe(userData => (this.userData = userData));
+  // }
+
+  // changeData(event) {
+  //   var msg = event.target.value;
+  //   this.user.changeData(msg);
+  // }
+  // login(data) {
+  //   this.user.changeData(data);
+  // }
 }
